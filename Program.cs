67921122d3e2fa5d610bc037dc73c84cdc6585b1
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-08-18 16:34:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Slot_Machine // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {9,5,1};
+            foreach (int i in numbers){Console.WriteLine(i);}
+        }
+    }
+}
