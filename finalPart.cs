d164e5288e72bdbd50584eeea2fe08ae6a89d87d
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2024-02-09 02:27:59 +0100

    divided program in different files

---
+++ finalPart.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Machine
+{
+    internal class finalPart
+    {
+        public static void final()
+        {
+            /*/final part of game*/
+
+
+            ConsoleKeyInfo userAnswer = Console.ReadKey(true);
+            Char useranswerChar = userAnswer.KeyChar;
+
+            if (cashBox > 0)
+            {
+
+                Console.WriteLine("\nDo you want to play again? y/n");
+                Console.ReadKey(true);
+
+                if (useranswerChar == NO_CHAR)
+                {
+                    break;
+                }
+                else
+                {
+                    cashBox -= PLAYING_COST;
+                }
+                Console.WriteLine(userChoiceText);
+                ConsoleKeyInfo userNumberChoice = Console.ReadKey(true);
+                userChoiceChar = userNumberChoice.KeyChar;
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("You don't have money! do you want to bet again? y/n");
+
+                ConsoleKeyInfo userConfirmation = Console.ReadKey(true);
+                char userConfirmationChar = userConfirmation.KeyChar;
+
+                if (userConfirmationChar == YES_CHAR)
+
+                {
+                    Console.WriteLine($"amount that you want to bet ");
+                    int lossConfirmation = Int32.Parse(Console.ReadLine());
+                    cashBox = lossConfirmation;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+        }
+
+    }
+}
---
+++ rows.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Machine
+{
+    internal class rows
+    {
+        public static void rows()
+        {
+            //++++++***** R O W S *******++++
+
+            if (ROWS_CHAR == userChoiceChar)
+            {
+                // 3 - check if numbers of a row are all the same
+
+                Console.WriteLine($"\n \nyour total amount of money is {cashBox}$");
+
+                for (int rowCheck = 0; rowCheck < slotNumbers.GetLength(0); rowCheck++)
+                {
+                    // switch on again
+                    winningMatchFound = true;
+
+                    for (int colCheck = 0; colCheck < slotNumbers.GetLength(1); colCheck++)
+                    {
+                        if (slotNumbers[rowCheck, 0] != slotNumbers[rowCheck, colCheck])
+                        {
+                            winningMatchFound = false;
+                            break;
+                        }
+                    }
+
+                    //handling of gameStatus      
+                    if (winningMatchFound)
+                    {
+                        numberOfMatches++;
+                    }
+                }
+
+                // 4 - output win / lose
+
+                if (numberOfMatches == 0)
+                {
+                    Console.WriteLine("You Lose!");
+                }
+                else
+                {
+                    Console.WriteLine($"You Win! number of matching rows: {numberOfMatches} ");
+                }
+
+                // 5 - keep track of money
+                if (numberOfMatches > 0)
+                {
+                    cashBox += CASHBOX_WIN;
+                }
+                else if (numberOfMatches == ROWS - 1)
+                {
+                    cashBox += BONUS_WIN;
+                }
+                else if (numberOfMatches == ROWS)
+                {
+                    cashBox += (BONUS_WIN * ROWS);
+                }
+                else
+                {
+                    cashBox -= CASHBOX_LOSS;
+                }
+            }
+        }
+
+    }
+}
